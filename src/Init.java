d88ed027cf0c13a021c143b6d0d77d71a04b43a1
DilipKunderu <dilipkunderu@gmail.com>
2017-12-10 10:05:11 -0800

    New remote how?
    Signed-off-by: Dilip Kunderu <dilipkunderu@gmail.com>

--- src/Init.java
+++ src/Init.java
@@ -1,8 +1,10 @@
 public class Init {
     // Display a message, preceded by the name of the current thread
+    static void testGit() {
+        System.out.println("blah blah");
+    }
     static void threadMessage(String message) {
-        String threadName =
-                Thread.currentThread().getName();
+        String threadName = Thread.currentThread().getName();
         System.out.format("%s: %s%n", threadName, message);
     }
 
