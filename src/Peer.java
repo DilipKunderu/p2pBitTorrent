955885259b4e4487a914771eee651381ce82abc9
DilipKunderu <dilipkunderu@gmail.com>
2018-03-11 12:48:35 -0400

    Singleton style for peer creation

--- src/Peer.java
+++ src/Peer.java
@@ -4,16 +4,6 @@ public class Peer {
     private int _port;
     private int _hasFile;
 
-    public Peer() {
-    }
-
-    public Peer(int _peerID, String _hostName, int _port, int _hasFile) {
-        this._peerID = _peerID;
-        this._hostName = _hostName;
-        this._port = _port;
-        this._hasFile = _hasFile;
-    }
-
     public int get_peerID() {
         return _peerID;
     }
@@ -44,5 +34,22 @@ public class Peer {
 
     public void set_hasFile(int _hasFile) {
         this._hasFile = _hasFile;
+    }
+
+    private static volatile Peer peer;
+
+    private Peer() {
+        if (peer != null) {
+            throw new RuntimeException("Use getPeerInstance() for a single instance");
+        }
+    }
+
+    public static Peer getPeerInstance() {
+        if (peer == null) {
+            synchronized (Peer.class) {
+                if (peer == null) peer = new Peer();
+            }
+        }
+        return peer;
     }
 }
