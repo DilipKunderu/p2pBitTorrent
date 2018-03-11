10b2c6b5030645921e6221e031ed760696265302
DilipKunderu <dilipkunderu@gmail.com>
2018-03-11 03:18:07 -0400

    server and client continuity commit 2

--- src/Server.java
+++ src/Server.java
@@ -34,6 +34,7 @@ public class Server {
         private String message;    //message received from the client
         private String MESSAGE;    //uppercase message send to the client
         private Socket connection;
+
         private ObjectInputStream in;    //stream read from the socket
         private ObjectOutputStream out;    //stream write to the socket
         private int no;        //The index number of the client
