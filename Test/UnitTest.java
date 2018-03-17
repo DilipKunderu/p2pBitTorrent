cf6c99789446cd12d5c808b628809414da62db34
suryaprasanna <prasannakumar006@gmail.com>
2018-03-17 12:44:27 -0400

    comment test class

--- Test/UnitTest.java
+++ Test/UnitTest.java
@@ -1,10 +1,11 @@
 import org.junit.jupiter.api.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTest {
-    @Test
-    void myFirstTest() {
-        assertEquals(2, 1 + 1);
-    }
+//    @Test
+//    void myFirstTest() {
+//        assertEquals(2, 1 + 1);
+//    }
 }
