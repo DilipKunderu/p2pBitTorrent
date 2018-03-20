d461db77a355695a9c4a1929e415b18f7797e125
hareesh94 <nutalapatihareesh@gmail.com>
2018-03-20 06:42:58 -0400

    Added get and send file methods

--- src/FileProcessor/FileManager.java
+++ src/FileProcessor/FileManager.java
@@ -1,16 +1,15 @@
 package FileProcessor;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
 
 public class FileManager {
     Map<Integer,File> pieceMap;
-
+    Map<Integer,File> fileSoFar = new TreeMap<>();
     private void fileSplit(File inputFile, int pieceSize){
         pieceMap = new HashMap<>();
+
         FileInputStream inputStream;
         FileOutputStream partOfFile;
         File newFilePart;
@@ -27,12 +26,12 @@ public class FileManager {
                 count++;
                 newFilePart = new File("Part"+Integer.toString(count));
                 partOfFile = new FileOutputStream(newFilePart);
-                pieceMap.put(count,newFilePart);
                 partOfFile.write(filePiece);
+                pieceMap.put(count,newFilePart);
                 partOfFile.flush();
                 partOfFile.close();
             }
-
+            inputStream.close();
 
         } catch (java.io.IOException e) {
             e.printStackTrace();
@@ -40,12 +39,68 @@ public class FileManager {
 
     }
 
-    private void getFilePart(int filePart){
-
+    private void sendFilePart(int filePart, Socket socket) throws IOException {
+        File fileToSend;
+        FileInputStream fileInputStream;
+        BufferedInputStream bufferedInputStream;
+        OutputStream outputStream;
+        fileToSend = pieceMap.get(filePart);
+        byte[] byteFile = new byte[(int) fileToSend.length()];
+        fileInputStream = new FileInputStream(fileToSend);
+        bufferedInputStream = new BufferedInputStream(fileInputStream);
+        bufferedInputStream.read(byteFile,0,byteFile.length);
+        outputStream = socket.getOutputStream();
+        outputStream.write(byteFile,0,byteFile.length);
+        outputStream.flush();
     }
 
-    private void merge(){
+    private void getFilePart(int filePart, Socket socket) throws IOException {
+        int fileSize=10000000;                                         //resolve the filesize
+        FileOutputStream fileOutputStream;
+        InputStream inputStream;
+        BufferedOutputStream bufferedOutputStream;
+        File fileToWrite;
+        int bytesRead, currTotal;
+        byte[] byteFile = new byte[fileSize];
+        inputStream = socket.getInputStream();
+        fileToWrite = new File("Part"+Integer.toString(filePart));
+        fileOutputStream = new FileOutputStream(fileToWrite);
+        bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bytesRead = inputStream.read(byteFile,0,byteFile.length);
+        currTotal = bytesRead;
+        do {
+            bytesRead = inputStream.read(byteFile, currTotal, byteFile.length - currTotal);
+            if (bytesRead >= 0) currTotal += bytesRead;
+        }while(bytesRead>-1);
+        bufferedOutputStream.write(byteFile,0,currTotal);
+        fileSoFar.put(filePart,fileToWrite);
+        bufferedOutputStream.flush();
+        bufferedOutputStream.close();
+    }
 
+    private void filesmerge(){
+        File mergeFile = new File("SharedFile");    // change file name
+        FileOutputStream fileOutputStream;
+        FileInputStream fileInputStream;
+        byte[] fileBytes;
+        int bytesRead=0;
+        try{
+            fileOutputStream = new FileOutputStream(mergeFile,true);
+            Set<Integer> keys = fileSoFar.keySet();
+
+            for(Integer key : keys){
+                fileInputStream = new FileInputStream(fileSoFar.get(key));
+                fileBytes = new byte[(int)fileSoFar.get(key).length()];
+                bytesRead = fileInputStream.read(fileBytes,0,(int) fileSoFar.get(key).length());
+                fileOutputStream.write(fileBytes);
+                fileOutputStream.flush();
+                fileInputStream.close();
+            }
+            fileOutputStream.close();
+
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) {
         FileManager fm = new FileManager();
