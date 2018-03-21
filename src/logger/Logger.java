8d20cb75124a8123ccf8beec6822b4baa98523e0
Meghana Madineni <meghanamadineni@gmail.com>
2018-03-21 16:37:38 -0700

    Logger commit 4

--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -11,23 +11,23 @@ public class Logger {
 
     int peer_ID;
 
-    protected final String defaultLogFile;
+    protected final String logFile;
 
     public Logger(int peer_ID){
         this.peer_ID = peer_ID;
-        this.defaultLogFile = "\\project\\log_peer_"+peer_ID+".log";
+        this.logFile = "\\project\\log_peer_"+peer_ID+".log";
     }
 
     public void log(String s) throws IOException {
-        log(defaultLogFile, s);
+        log(logFile, s);
     }
 
     public void log(String f, String s) throws IOException {
-        TimeZone tz = TimeZone.getTimeZone("EST"); // or PST, MID, etc ...
-        Date now = new Date();
-        DateFormat df = new SimpleDateFormat("yyyy.mm.dd hh:mm:ss ");
-        df.setTimeZone(tz);
-        String currentTime = df.format(now);
+        TimeZone EST = TimeZone.getTimeZone("EST");
+        Date date = new Date();
+        DateFormat format = new SimpleDateFormat("yyyy.mm.dd hh:mm:ss ");
+        format.setTimeZone(EST);
+        String currentTime = format.format(date);
         FileWriter aWriter = new FileWriter(f, true);
         aWriter.write(currentTime + ": Peer " + peer_ID + s + "\n");
         aWriter.flush();
